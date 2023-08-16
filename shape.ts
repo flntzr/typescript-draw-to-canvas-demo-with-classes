d1b6aae44ed6a3b352481307653a56326c5bb547
flntzr <github@flintzer.com>
2023-08-16 16:13:36 +0200

    Implement ts class style

--- shape.ts
+++ shape.ts
@@ -1,24 +1,73 @@
-export interface Rectangle extends Point {
-  name: 'rectangle';
-  width: number;
-  height: number;
+export interface Shape {
+  draw(context: CanvasRenderingContext2D): void;
 }
 
-export interface Triangle {
-  name: 'triangle';
-  point1: Point;
-  point2: Point;
-  point3: Point;
+abstract class ColoredShape implements Shape {
+  public constructor(private _color: string) {}
+
+  public abstract draw(context: CanvasRenderingContext2D): void;
+
+  protected get color(): string {
+    return this._color;
+  }
 }
 
-export interface Circle extends Point {
-  name: 'circle';
-  radius: number;
+export class Rectangle extends ColoredShape {
+  public constructor(
+    private x: number,
+    private y: number,
+    private width: number,
+    private height: number,
+    color = 'rgba(200, 0, 0, 0.3)'
+  ) {
+    super(color);
+  }
+
+  public draw(context: CanvasRenderingContext2D): void {
+    context.fillStyle = this.color;
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+export class Triangle extends ColoredShape {
+  public constructor(
+    private point1: Point,
+    private point2: Point,
+    private point3: Point,
+    color = 'rgba(0, 200, 0, 0.3)'
+  ) {
+    super(color);
+  }
+
+  public draw(context: CanvasRenderingContext2D): void {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.moveTo(this.point1.x, this.point1.y);
+    context.lineTo(this.point2.x, this.point2.y);
+    context.lineTo(this.point3.x, this.point3.y);
+    context.fill();
+  }
+}
+
+export class Circle extends ColoredShape {
+  public constructor(
+    private x: number,
+    private y: number,
+    private radius: number,
+    color = 'rgba(0, 0, 200, 0.3)'
+  ) {
+    super(color);
+  }
+
+  public draw(context: CanvasRenderingContext2D): void {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    context.fill();
+  }
 }
 
 export interface Point {
   x: number;
   y: number;
 }
-
-export type Shape = Rectangle | Triangle | Circle;
